7333e003fa8abf1397b67013944ff2dc77fdf13c
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-05-14 13:20:45 +0100

    working on openCV

---
+++ external/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd opencv
+mkdir build
+cd build 
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+make -j8
