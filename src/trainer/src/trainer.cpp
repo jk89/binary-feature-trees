0311b42ebdc1ec82b3029443001e2748119e8efc
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-05-14 17:49:10 +0100

    reading voc file

--- src/trainer/src/trainer.cpp
+++ src/trainer/src/trainer.cpp
@@ -1,31 +1,57 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <iostream>
+#include <fstream>
+//#include <boost/algorithm/string.hpp>
+
 using namespace cv;
 using namespace std;
+using namespace boost::filesystem;
+
+namespace fs = boost::filesystem;
+
+
+const char *filename = "./data/ORBvoc.txt";
+
+/**
+ * \brief   Return the filenames of all files that have the specified extension
+ *          in the specified directory and all subdirectories.
+ */
+std::vector<fs::path> get_all(fs::path const & root) // , std::string const & ext
+{
+    std::vector<fs::path> paths;
+    if (fs::exists(root) && fs::is_directory(root))
+    {
+        for (auto const & entry : fs::directory_iterator(root)) // recursive_directory_iterator
+        {
+            if (fs::is_regular_file(entry)) //  && entry.path().extension() == ext
+                {
+                    cout << entry.path().filename() << endl;
+                    paths.emplace_back(entry.path().filename());
+                };
+        }
+    }
+
+    return paths;
+}
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    std::ifstream file(filename);
+    // file.open(filename, ios::in|ios::out|ios::binary);
+    get_all("./data");
+    cout << fs::exists(filename) << endl;
+    cout << filename << endl;
+    cout << file.is_open() << endl;
+    if (file.is_open())
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+        std::string line;
+        while (std::getline(file, line))
+        {
+            // using printf() in all tests for consistency
+            // printf("%s", line.c_str());
+            cout << line.c_str() << endl;
+        }
+        file.close();
     }
-    Mat image;
-    image = imread(argv[1], 1);
-    if (!image.data)
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-
-    string line("test\ttest2\ttest3");
-    vector<string> strs;
-    boost::split(strs, line, boost::is_any_of("\t"));
-    cout << "* size of the vector: " << strs.size() << endl;
-
-    imshow("Display Image", image);
-    waitKey(0);
-    return 0;
 }
