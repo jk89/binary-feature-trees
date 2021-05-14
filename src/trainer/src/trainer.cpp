1b760257854e0c6ea6602846f87bb29bebf0c96d
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-05-14 14:01:50 +0100

    working install

--- src/trainer/src/trainer.cpp
+++ src/trainer/src/trainer.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
 using namespace std;
 
-int main()
+
+
+int main(int argc, char** argv )
 {
-    cout << "Hello World2" << endl;
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    Mat image;
+    image = imread( argv[1], 1 );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
 }
