b05beec7531c31d081693f46c1f7248e8ec4bd9f
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-06-11 17:38:48 +0100

    cleanup

--- src/trainer/src/trainer.cpp
+++ src/trainer/src/trainer.cpp
@@ -6,8 +6,6 @@
 #include <fstream>
 #include <stdlib.h> /* atoi */
 
-//#include <boost/algorithm/string.hpp>
-
 // using namespace cv;
 using namespace std;
 using namespace boost::filesystem;
@@ -16,14 +14,6 @@ using namespace std::chrono;
 namespace fs = boost::filesystem;
 
 const char *filename = "./data/ORBvoc.txt";
-
-struct compareMats
-{
-    bool operator()(const cv::Mat &a, const cv::Mat &b) const
-    {
-        return sum(b - a)[0] > 0;
-    }
-};
 
 struct compareFeatureVecs
 {
@@ -66,19 +56,17 @@ std::vector<fs::path> get_all(fs::path const &root) // , std::string const & ext
     return paths;
 }
 
-int main(int argc, char **argv)
+cv::Mat load_data()
 {
+    cv::Mat m;
     std::ifstream file(filename);
     // file.open(filename, ios::in|ios::out|ios::binary);
     get_all("./data");
-    cout << fs::exists(filename) << endl;
-    cout << filename << endl;
-    cout << file.is_open() << endl;
     if (file.is_open())
     {
         std::string line;
         int idx = 0;
-        vector<array<uint8_t, 32>> data = {}; // uint8_t[32]
+        vector<array<uint8_t, 32>> data = {};
         // for each line in file
         while (std::getline(file, line))
         {
@@ -96,9 +84,7 @@ int main(int argc, char **argv)
             strs.erase(strs.begin());
             strs.erase(strs.begin() + 34);
             strs.erase(strs.begin() + 34);
-            // cout << "* size of the vector: " << strs.size() << endl;
             // decode feature
-            // uint8_t feature[32] = {};
             array<uint8_t, 32> feature = {};
             int featureIdx = 0;
             for (auto it = strs.begin(); it != strs.end(); ++it)
@@ -107,112 +93,40 @@ int main(int argc, char **argv)
                 int icfeature = std::stoi(cfeature);
                 uint8_t ui8feature = icfeature;
                 feature[featureIdx] = ui8feature;
-
-                // cout << unsigned(feature[featureIdx]) << " ";
                 featureIdx++;
             }
-
+            // push feature to data vec
             data.push_back(feature);
-
-            // Mat mFeature(32, 1, CV_64FC1, a)
-
-            /*
-                double a[] = { 1,2,3,4,5,6, 7,8,9 }; //matrix m0
-                double b[] = { 2,4,5,6,8,10,12,13,1 };  //matrix m1
-                // rows, cols, format, data
-                Mat m0(3, 3, CV_64FC1, a);
-                Mat m1(3, 3, CV_64FC1, b);
-            */
-
             idx++;
         }
         file.close();
-        cout << "loaded " << data.size() << " ORB vectors" << endl;
-
-        cout << " starting to deduplicate " << endl;
-
-        // compareFeatureVecs
-        // create set
+        cout << " Loaded file into vector " << endl;
+        
+        // create set for deduplication
         std::set<array<uint8_t, 32>, compareFeatureVecs> dedupedSetData;
-
         for (int i = 0; i < data.size(); ++i)
         {
             dedupedSetData.insert(data[i]);
         }
-
-        cout << " deduplicate finished " << dedupedSetData.size() << endl;
+        cout << " Deduplicated features " << endl;
 
         // convert back to vector now its all unique
-
         vector<array<uint8_t, 32>> dedupVectorData = {}; // uint8_t[32]
-
         dedupVectorData.assign(dedupedSetData.begin(), dedupedSetData.end());
+        cout << " Converted unique feature set into vector " << endl;
 
-        cout << " converted back to a vector " << dedupedSetData.size() << endl;
-
-        cv::Mat m = cv::Mat(dedupVectorData.size(), 32, CV_8U); // row col
+        m = cv::Mat(dedupVectorData.size(), 32, CV_8U); // row col
         memcpy(m.data, dedupVectorData.data(), dedupVectorData.size() * sizeof(array<uint8_t, 32>));
-
-        cout << " converted to a matrix " << endl;
-
-        cout << "details of m rows" << m.rows << " cols " << m.cols << " tyoe " << m.type() << endl;
-        cout << "first row: " << cv::format(m.row(0), cv::Formatter::FMT_PYTHON) << endl;
-        cout << "second row: " << cv::format(m.row(1), cv::Formatter::FMT_PYTHON) << endl;
-
-        // convert to array
-        // vector<uint8_t>
-
-        // uint8_t *a = data.data(); // &data[0];
-
-        // cout << "converted to arr " << endl;
-        // cout << "done " << endl;
-
-        // cv::Mat m = cv::Mat(data.size(), 32, CV_8U); // row col
-
-        // memcpy(m.data, data.data(), data.size() * sizeof(array<uint8_t, 32>));
-
-        // cout << "details of m rows" << m.rows << " cols " << m.cols << " tyoe " << m.type() << endl;
-        // cout << "aaa: " << cv::format(m.row(0), cv::Formatter::FMT_PYTHON) << endl;
-        // cout << "aaa: " << cv::format(m.row(1), cv::Formatter::FMT_PYTHON) << endl;
-
-        // std::set<cv::Mat, compareMats> nonDuplicatedPairs;
-
-        /*for (int i = 0; i < m.rows; ++i)
-        {
-            nonDuplicatedPairs.insert(m.row(i));
-        }*/
-
-        // cout << " unique features " << nonDuplicatedPairs.size() << endl;
-
-        // unique vector
-        /*
-theVector.assign(theSet.begin(), theSet.end());
-        */
-        /*vector<cv::Mat> dedup_vec = {};
-
-        // Iterate till the end of set
-        std::set<cv::Mat, compareMats>::iterator it = nonDuplicatedPairs.begin();
-        while (it != nonDuplicatedPairs.end())
-        {
-            // Print the element
-            std::cout << cv::format((*it), cv::Formatter::FMT_PYTHON) << " , ";
-            //Increment the iterator
-            it++;
-        }*/
-        // dedup_vec.assign()
-
-        // cout << "M: " << cv::format(nonDuplicatedPairs, cv::Formatter::FMT_PYTHON) << endl;
-
-        // Mat m0(3, 3, CV_64FC1, a);
-
-        // process data.. data
-        // Mat m0(3, 3, CV_64FC1, a)
+        cout << " Loaded vector into matrix " << endl;
     }
+    return m;
+}
 
-    /*std::chrono::Timer clock; // Timer<milliseconds, steady_clock>
-
-    clock.tick();
-    clock.tock();*/
-
-    // cout << "Run time = " << clock.duration().count() << " ms\n";
+int main(int argc, char **argv)
+{
+    cv::Mat m;
+    m = load_data();
+    cout << "details of m rows" << m.rows << " cols " << m.cols << " tyoe " << m.type() << endl;
+    cout << "first row: " << cv::format(m.row(0), cv::Formatter::FMT_PYTHON) << endl;
+    cout << "second row: " << cv::format(m.row(1), cv::Formatter::FMT_PYTHON) << endl;
 }
