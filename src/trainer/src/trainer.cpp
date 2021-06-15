cdef682b51828e56d31b142e5d8deca43cf943fc
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-06-15 14:25:23 +0100

    working on concurrency range calculator

--- src/trainer/src/trainer.cpp
+++ src/trainer/src/trainer.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h> /* atoi */
+#include <thread>
 
 // using namespace cv;
 using namespace std;
@@ -14,6 +15,7 @@ using namespace std::chrono;
 namespace fs = boost::filesystem;
 
 const char *filename = "./data/ORBvoc.txt";
+const auto processor_count = std::thread::hardware_concurrency();
 
 struct compareFeatureVecs
 {
@@ -101,7 +103,7 @@ cv::Mat load_data()
         }
         file.close();
         cout << " Loaded file into vector " << endl;
-        
+
         // create set for deduplication
         std::set<array<uint8_t, 32>, compareFeatureVecs> dedupedSetData;
         for (int i = 0; i < data.size(); ++i)
@@ -122,60 +124,73 @@ cv::Mat load_data()
     return m;
 }
 
-
 int random(int min, int max) //range : [min, max]
 {
-   static bool first = true;
-   if (first) 
-   {  
-      srand( time(NULL) ); //seeding for the first time only!
-      first = false;
-   }
-   return min + rand() % (( max + 1 ) - min);
+    static bool first = true;
+    if (first)
+    {
+        srand(time(NULL)); //seeding for the first time only!
+        first = false;
+    }
+    return min + rand() % ((max + 1) - min);
 }
 
-int hammingDistance(cv::Mat v1, cv::Mat v2) {
-    cout << "v1 " << cv::format(v1, cv::Formatter::FMT_PYTHON) << endl;
+int hammingDistance(cv::Mat v1, cv::Mat v2)
+{
+    /*cout << "v1 " << cv::format(v1, cv::Formatter::FMT_PYTHON) << endl;
     cout << "v1 rows " << v1.rows << endl;
     cout << "v1 cols " << v1.cols << endl;
 
     cout << "v2 " << cv::format(v2, cv::Formatter::FMT_PYTHON) << endl;
     cout << "v2 rows " << v2.rows << endl;
-    cout << "v2 cols " << v2.cols << endl;
+    cout << "v2 cols " << v2.cols << endl;*/
 
     // result += popCountTable[a[i] ^ b2[i]];
     // bitwise_xor(drawing1,drawing2,res);
     // float * testData1D = (float*)testDataMat.data;
     // cv::Mat distance;
     // cv::bitwise_xor(v1, v2, distance);
-    int distance = cv::norm(v1, v2 ,cv::NORM_HAMMING);
+    int distance = cv::norm(v1, v2, cv::NORM_HAMMING);
 
-    cout << " dist " << distance << endl;
-    return 0;
+    return distance;
+
+    // cout << " dist " << distance << endl;
+    // return 0;
 }
 
-void seedKernel(cv::Mat data, int currentCentroidIndex, vector<int> centroids, int k) // data, dataIdValue, centroids, k, metric
+int seedKernel(cv::Mat data, int currentCentroidIndex, vector<int> centroids, int k) // data, dataIdValue, centroids, k, metric
 {
     cout << "seed kernel " << currentCentroidIndex << endl;
+    int maxDistance = 0;
+    int maxIndex = -1;
     // for the whole dataset of features
-    for (int r = 0; r < data.rows; r++) {
-        cout << "  r " << r << endl;
+    for (int r = 0; r < data.rows; r++)
+    {
+        // cout << "  r " << r << endl;
         // extract feature from row index
         cv::Mat currentDatasetFeature = data.row(r);
         int minDistance = INT_MAX;
         // for each already selected centroid
-        for (int c = 0; c < centroids.size(); c++) {
-            cout << " c " << c << endl;
+        for (int c = 0; c < centroids.size(); c++)
+        {
+            // cout << " c " << c << endl;
             int currentCentroidDataSetIndex = centroids[c];
             cv::Mat currentCentroidFeature = data.row(currentCentroidDataSetIndex);
             // currentDatasetFeature
-            hammingDistance(currentDatasetFeature, currentCentroidFeature);
-
+            const int distance = hammingDistance(currentDatasetFeature, currentCentroidFeature);
+            // min minDistance
+            minDistance = min(distance, minDistance);
+        }
+        if (minDistance > maxDistance)
+        {
+            maxDistance = minDistance;
+            maxIndex = r;
         }
     }
+    return maxIndex; // return the best centroid
 }
 
-void seedClusters(cv::Mat data, int k) // data, k, metric
+vector<int> seedClusters(cv::Mat data, int k) // data, k, metric
 {
     // largest index
     const int dataLength = data.rows;
@@ -185,8 +200,42 @@ void seedClusters(cv::Mat data, int k) // data, k, metric
     centroids.push_back(random(0, dataLength - 1));
 
     // for the rest of k
-    for (int i = 0; i < k -1; i++) {
-        seedKernel(data, i, centroids, k);
+    for (int i = 0; i < k - 1; i++)
+    {
+        const int nextCentroid = seedKernel(data, i, centroids, k);
+        centroids.push_back(nextCentroid);
+    }
+
+    return centroids;
+}
+
+void optimiseClusterMembership(cv::Mat data, int k, vector<int> centroidSeedIndices)
+{ // data, n=4, metric=hammingVector, intitalClusterIndices=None
+    // centroidData
+    // processor_count
+    const int dataRowCount = data.rows;
+    const int threadPool = processor_count;
+    const int batchSize = (int)dataRowCount / threadPool;
+    int lastBatchSize = batchSize;
+    const int globalRemainder = dataRowCount - (batchSize * threadPool);
+    lastBatchSize = lastBatchSize + globalRemainder;
+    cout << " thread pool " << threadPool << endl;
+    cout << " batch size " << batchSize << endl;
+    cout << " last batch " << lastBatchSize << endl;
+    cout << " total " << dataRowCount << endl;
+
+    // so what are the ranges
+    for (int i = 0; i < threadPool; i++)
+    {
+        int rangeStart = i * batchSize;
+        int rangeEnd = ((i + 1) * batchSize) - 1;
+        if (i == threadPool - 1)
+        {
+            // isLast = true;
+            rangeEnd = rangeEnd + globalRemainder;
+
+        }
+        cout << "start " << rangeStart << " end " << rangeEnd << endl;
     }
 }
 
@@ -197,5 +246,9 @@ int main(int argc, char **argv)
     cout << "details of m rows" << m.rows << " cols " << m.cols << " tyoe " << m.type() << endl;
     cout << "first row: " << cv::format(m.row(0), cv::Formatter::FMT_PYTHON) << endl;
     cout << "second row: " << cv::format(m.row(1), cv::Formatter::FMT_PYTHON) << endl;
-    seedClusters(m, 8);
+    const vector<int> centroids = seedClusters(m, 8);
+    for (auto i = centroids.begin(); i != centroids.end(); ++i)
+        std::cout << *i << ' ';
+
+    optimiseClusterMembership(m, 8, centroids);
 }
